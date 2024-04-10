661f1439dc6c99e4eb0a2781991cba3376b762f1
Jefferson <40587602+jefftdb@users.noreply.github.com>
2024-04-10 12:30:59 -0300

    Adicionei a relação manytoMany ingredients-Recipe

--- recipes/admin.py
+++ recipes/admin.py
@@ -16,8 +16,9 @@ class IngredientAdmin(admin.ModelAdmin):
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("title","description","slug","preparation_time","preparation_time_unity","servings_time",
                     "servings_unity","preparation_steps","preparation_steps_is_html","created_at","updated_at",
-                    "is_published","cover","category","author","ingredients")
+                    "is_published","cover","category","author","get_ingredients")
     search_fields = ("title","description","ingredients","category")
+    
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Measure, MeasureAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -7,7 +7,6 @@ class Measure(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=65)
 
-
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=65)
@@ -35,6 +34,14 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    ingredients = models.ForeignKey(Ingredient, on_delete=models.SET_NULL, null=True)
+    ingredients = models.ManyToManyField(Ingredient,related_name="ingredients")
+
+    def __str__(self):
+        return self.model
+    
+    def get_ingredients(self, obj):
+        return "\n".join([p.ingredient for p in obj.ingredients.all()])
+
+
 
 
